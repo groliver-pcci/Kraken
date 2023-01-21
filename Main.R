ab46683dbfa0a41c0aee292d4408ce989aa76845
BoRocks813 <72171117+BoRocks813@users.noreply.github.com>
2023-01-20 22:59:16 -0600

    1.1
    Updated match page and began upload page

--- Main.R
+++ Main.R
@@ -1,42 +1,78 @@
 library(shiny)
 
 ui <- navbarPage(
+  
   "Hydra",
   
   tabPanel("Input Files",
            fluidPage(
-             fileInput(
-               "file",
-               "Upload CSV",
-               multiple = FALSE
+             column(fluidRow(
+             sidebarPanel(
+                fileInput(
+                  "file",
+                  "Upload CSV",
+                   multiple = FALSE
+                ),
+                width = 12
+             )),
+             fluidRow(
+               sidebarPanel(
+                 titlePanel("Apply Data?"),
+                 actionButton("yesData", "Yes", width = '60px'),
+                 actionButton("noData", "No", width = '60px'),
+                 width = 12
+               )
+             ),
+             width = 3
+             ),
+             mainPanel(
+               
              )
            )
   ),
   
   tabPanel("Teams",
            fluidPage(
-             sidebarPanel(
-             textInput(
-               "search",
-               "Search:",
-               placeholder = "enter team or match number"
+             column(fluidRow(
+               sidebarPanel(
+                 textInput("search",
+                           "Search:",
+                           placeholder = "enter team number"),
+                 verbatimTextOutput("text"),
+                 
+                 
+                 width = 12
+               )
              ),
-             verbatimTextOutput("text")),
-             
-           )
-           ),
+             fluidRow(sidebarPanel(
+               titlePanel("hello"),
+               width = 12
+             )),
+             width = 3),
+             column(mainPanel(plotOutput("graph")),
+                    width = 9),
+           )), 
   
-  tabPanel("Matches")
+  tabPanel("Matches"),
   
   tabPanel("Graph"),
   
-  selected = "Search"
+  tabPanel("Match Planner"),
+  
+  tabPanel("Import Data"),
+  
+  tabPanel("Export Data"),
+  
+  selected = "Teams"
 )
 
 server <- function(input, output) {
     
     output$text <- renderText({input$search})
-  
+    
+    output$graph <- renderPlot({
+      hist(c(3, 4, 5, 7))
+    })
 }
 
 shinyApp(ui, server)
