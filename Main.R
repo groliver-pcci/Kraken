45036000e626e35d31ee23960ddfc15607c2305a
BoRocks813 <72171117+BoRocks813@users.noreply.github.com>
2023-01-28 11:32:57 -0600

    1.6
    pulls from statbotics

--- Main.R
+++ Main.R
@@ -3,15 +3,78 @@ library(DT)
 library(tidyverse)
 library(scales)
 library(formattable)
+library(devtools)
+load_all("C:\\Users\\wcbri\\Documents\\tbaR_1.0.1\\tbaR\\tbaR.Rproj")
 
+
+statbotics <- "https://api.statbotics.io/v2/"
+
+tbaKey <- "2022txirv"
+
+week <- 1
+
+year <- "2022"
 
 numCols <- 28
 
 dtSettings <- list(scrollX = TRUE, scrollY = TRUE, fixedColumns = list(leftColumns = c(2, 3)))
 
+updateTeamValues <- function() {
+  for(row in 1:length(vals$mainframe$teamNum)) {
+    teamNumber <- vals$mainframe[row, 1]
+    
+    
+  }
+}
+
+findTeamIndex <- function(teamNum) {
+  for(row in 1:length(vals$infoframe$teamNum)) {
+    if(toString(teamNum) == vals$infoframe$teamNum[row]) {
+      return(row)
+    }
+  }
+}
+
+
+getStatboticsTeam <- function(teamNum) {
+  link <- paste(statbotics, "team_year/", teamNum, "/", year, sep = "")
+  
+  team <- content(GET(link))
+}
+
+vals <- reactiveValues(
+  mainframe = data.frame(),
+  searchframe = data.frame(matrix(nrow = 0, ncol = numCols)),
+  previewframe = data.frame(),
+  
+  calcframe = data.frame(epa = c()
+  ),
+  
+  scheduleframe = data.frame(round = c(),
+                             match_number = c(),  
+                             red1 = c(),
+                             red2 = c(),
+                             red3 = c(),
+                             blue1 = c(),
+                             blue2 = c(),
+                             blue3 = c()
+  ),
+  
+  infoframe = data.frame(teamNum = c(),
+                         epa = c(),
+                         matchesPlayed = c()
+  ),
+  
+  constantframe = data.frame(teamNum = c(),
+                             k = c(),
+                             m = c()
+  )
+)
+
+
 ui <- navbarPage(
   
-  title = div(icon("gitkraken", lib = "font-awesome"), "  Kraken"),
+  title = div(icon("gitkraken", lib = "font-awesome", id="krakenicon"), "  Kraken"),
   
   tabPanel("Data",
            fluidPage(
@@ -44,7 +107,7 @@ ui <- navbarPage(
                )
              ),
              fluidRow(
-               h3("Clear Data"),
+               h3("Clear Data", id="clearDataText"),
                sidebarPanel(
                  h5("WARNING: this button will delete all current data. Consider exporting the data first."),
                  actionButton("deleteFiles", "Delete Files"),
@@ -90,11 +153,20 @@ ui <- navbarPage(
   
   tabPanel("Qualitative"),
   
-  tabPanel("Graph"),
+  tabPanel("Graph",
+           DTOutput("tbaDT")),
   
   tabPanel("Match Planner"),
   
-  tabPanel("TBA"),
+  tabPanel("TBA",
+           actionButton("getSchedule", "Get Match Schedule")),
+  
+  tabPanel("Statbotics",
+           actionButton("getStatbotics", "Get Statbotics Info"),
+           DTOutput("statboticsData")),
+  
+  tabPanel("Schedule",
+           DTOutput("matchScheduleDT")),
   
   selected = "Data",
   
@@ -105,7 +177,10 @@ ui <- navbarPage(
          
          #confirmDelete, #deleteFiles {
             background-color: #d41704;
-            color: white;
+         }
+         
+         #krakenicon {
+            color: #bb520a;
          }
          
          ')
@@ -116,10 +191,16 @@ server <- function(input, output) {
     
     f <- NA
     
+    output$tbaDT <- renderDT(event_matches("2022txirv"))
+    
+    output$statboticsData <- renderDT(vals$infoframe)
+    
     output$dataExport <- downloadHandler("scoutingdata.csv", 
                                          content = function(file) {
                                            write.csv(vals$mainframe, file)
                                          })
+    
+    output$matchScheduleDT <- renderDT(datatable(vals$scheduleframe, extensions = "FixedColumns", options = dtSettings))
     
     deleteModal <- function() {
       modalDialog(
@@ -140,6 +221,86 @@ server <- function(input, output) {
     }
     
     output$mainframeOutput <- renderDT(datatable(vals$mainframe, extensions = "FixedColumns", options = dtSettings))
+    
+    
+    observeEvent(input$getStatbotics, {
+      
+      teamsInfo <- event_teams(tbaKey)
+      
+      teamNums <- c()
+      
+      for(team in 1:length(teamsInfo$key)) {
+        teamNums <- append(teamNums, substr(teamsInfo$key[team], 4, 7))
+      }
+      
+      for(team in  1:length(teamsInfo$key)) {
+        t <- list(teamNum = NA,
+                  epa = NA,
+                  matchesPlayed = NA)
+        
+        teamNumber <- teamNums[team]
+        
+        teamInfo <- getStatboticsTeam(teamNumber)
+        
+        t$teamNum <- teamNumber
+        
+        if(!(is.null(teamInfo$epa_end))) {
+          t$epa <- teamInfo$epa_end
+        } else {
+          t$epa <- "NA"
+        }
+        
+        if(!(is.null(teamInfo$count))) {
+          t$matchesPlayed <- teamInfo$count
+        } else {
+          if(week == 1) {
+            t$matchesPlayed <- 0
+          } else {
+            t$matchesPlayed <- 5
+          }
+        }
+        
+        vals$infoframe <- rbind(vals$infoframe, t)
+      }
+      
+      
+      
+    })
+    
+    
+    observeEvent(input$getSchedule, {
+      
+      vals$scheduleframe = data.frame(round = c(),
+                                      match_number = c(),  
+                                      red1 = c(),
+                                      red2 = c(),
+                                      red3 = c(),
+                                      blue1 = c(),
+                                      blue2 = c(),
+                                      blue3 = c()
+      )
+      
+      tbaMatchListTemp <- event_matches("2022txirv")
+    
+      tbaMatchListTemp <- tbaMatchListTemp[order(tbaMatchListTemp$match_number), ]
+      
+      for(match in 1:nrow(tbaMatchListTemp)) {
+        if(tbaMatchListTemp$comp_level[match] == "qm") {
+          currentMatchTeams <- data.frame(round = tbaMatchListTemp$comp_level[match],
+                                          match_number = tbaMatchListTemp$match_number[match],
+                                          red1 = substr(tbaMatchListTemp$red1[match], 4, 7),
+                                          red2 = substr(tbaMatchListTemp$red2[match], 4, 7),
+                                          red3 = substr(tbaMatchListTemp$red3[match], 4, 7),
+                                          blue1 = substr(tbaMatchListTemp$blue1[match], 4, 7),
+                                          blue2 = substr(tbaMatchListTemp$blue2[match], 4, 7),
+                                          blue3 = substr(tbaMatchListTemp$blue3[match], 4, 7)
+                                          )
+          
+          vals$scheduleframe <- rbind(vals$scheduleframe, currentMatchTeams)
+        }
+      }
+      
+    })
     
     observeEvent(input$file, {
       d <- input$file
@@ -165,6 +326,11 @@ server <- function(input, output) {
         
         if(nrow(vals$mainframe) == 0) {
           vals$mainframe <- rbind(vals$mainframe, vals$previewframe[1, ])
+          
+          teamIndex <- findTeamIndex(vals$previewframe$teamNum[1])
+          
+          vals$infoframe$matchesPlayed[teamIndex] <- vals$infoframe$matchesPlayed[teamIndex] + 1
+          
           vals$previewframe <- data.frame()
           return(NULL)
         } else {
@@ -178,10 +344,13 @@ server <- function(input, output) {
           }
         }
         
-        print(repeatFound)
-        
         if(repeatFound == FALSE) {
           vals$mainframe <- rbind(vals$mainframe, vals$previewframe[1, ])
+          
+          teamIndex <- findTeamIndex(vals$previewframe$teamNum[1])
+          
+          vals$infoframe$matchesPlayed[teamIndex] <- vals$infoframe$matchesPlayed[teamIndex] + 1
+          
           vals$previewframe <- data.frame()
         }
           
@@ -240,13 +409,6 @@ server <- function(input, output) {
     }
       
     )
-    
-    vals <- reactiveValues(
-      mainframe = data.frame(),
-      searchframe = data.frame(matrix(nrow = 0, ncol = numCols)),
-      previewframe = data.frame()
-    )
-    
     
 }
 
